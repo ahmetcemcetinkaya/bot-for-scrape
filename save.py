c2a388cd82cdd6e813ec52c2b371f55f3264d3f0
ahmet <ahmetcemcetinkaya@gmail.com>
2016-07-22 02:43:06 +0300

    Update save.py

--- save.py
+++ save.py
@@ -5,11 +5,44 @@ import io
 from os import path
 import urllib
 import urllib2
+import MySQLdb
+
+def mysqlbaglan(names,exts):  # Database data save function.
+    ad = names[:-1]
+    
+    dbsa = MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
+                         user="root",         # your username
+                         passwd="",  # your password
+                         db="deneme")        # name of the data base
+
+
+    try:
+        cur = dbsa.cursor()
+        print names, exts
+        cur.execute("insert into kl values('"+ad+"','"+exts+"')")
+        dbsa.commit()
+    except MySQLdb.Error, e:
+        try:
+            print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+        except IndexError:
+            print "MySQL Error: %s" % str(e)
+    
+def download_very_big_image(urls,location):
+    url = urls
+    filename = location
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib2.Request(url,headers=hdr)
+    conn = urllib2.urlopen(req)
+    output = open(filename, 'wb') #binary flag needed for Windows
+    output.write(conn.read())
+    output.close()
+
 
 def creates(namea,detailss,picture):
     ozet = detailss
     yeni_yol = namea[:-1]
     anime = r'C:\\Users\\ahmetwww\\Desktop\\Animeler\\%s' % (yeni_yol)
+    
     
 
     try: 
@@ -24,41 +57,15 @@ def creates(namea,detailss,picture):
 
         if not (os.path.isfile(name)):
             with open(name,"w") as f: # Try to write text to txt file
+                f.truncate()
                 f.write(ozet)
                 f.close()
 
 
-        try:
-            db(yeni_yol,ozet) # Write data to database.
-        except:
-            print "database hatasi"
+       
 
 
  
 
     except:
         print "Genel hata"
-
-
-def download_very_big_image(urls,location):
-    url = urls
-    filename = location
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib2.Request(url,headers=hdr)
-    conn = urllib2.urlopen(req)
-    output = open(filename, 'wb') #binary flag needed for Windows
-    output.write(conn.read())
-    output.close()
-
-def db(names,exts):  # Database data save function.
-    import MySQLdb
-
-    db = MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="",  # your password
-                     db="deneme")        # name of the data base
-   
-    cur = db.cursor()
-    cur.execute("insert into kl values(%s,%d)" % (names,exts))
-    db.commit()
-    
